e52a9295b201a82b038ecf7e8cc4dc459b2c15b2
Martín <magadiflo@gmail.com>
2023-11-08 19:58:54 -0500

    Actualizando imagen de un deployment

--- business-domain/dk-ms-users/src/main/java/com/magadiflo/dk/business/domain/users/app/controllers/UserController.java
+++ business-domain/dk-ms-users/src/main/java/com/magadiflo/dk/business/domain/users/app/controllers/UserController.java
@@ -3,26 +3,40 @@ package com.magadiflo.dk.business.domain.users.app.controllers;
 import com.magadiflo.dk.business.domain.users.app.models.entity.User;
 import com.magadiflo.dk.business.domain.users.app.services.IUserService;
 import jakarta.validation.Valid;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/api/v1/users")
 public class UserController {
     private final IUserService userService;
+    private final ApplicationContext context;
 
-    public UserController(IUserService userService) {
+    public UserController(IUserService userService, ApplicationContext context) {
         this.userService = userService;
+        this.context = context;
+    }
+
+    @GetMapping(path = "/cause-error")
+    public void causeError() {
+        ((ConfigurableApplicationContext) this.context).close();
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
-        return ResponseEntity.ok(this.userService.findAllUsers());
+    public ResponseEntity<Map<String, Object>> getAllUsers() {
+        Map<String, Object> body = new HashMap<>();
+        body.put("users", this.userService.findAllUsers());
+        body.put("message", "Lista de todos los usuarios");
+        return ResponseEntity.ok(body);
     }
 
     @GetMapping(path = "/{id}")
