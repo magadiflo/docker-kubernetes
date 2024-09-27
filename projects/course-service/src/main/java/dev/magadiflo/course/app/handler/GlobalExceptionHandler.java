dd58f3f1f7ff7bcca6951393dad48ff48405d3f2
Martín <magadiflo@gmail.com>
2024-09-26 23:57:55 -0500

    Manejo de excepciones

---
+++ projects/course-service/src/main/java/dev/magadiflo/course/app/handler/GlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package dev.magadiflo.course.app.handler;
+
+import dev.magadiflo.course.app.exception.CourseNotFoundException;
+import dev.magadiflo.course.app.exception.NotFoundException;
+import dev.magadiflo.course.app.util.HttpErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CourseNotFoundException.class)
+    public ResponseEntity<HttpErrorResponse> handleNotFoundException(NotFoundException exception,
+                                                                     HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(HttpErrorResponse.builder()
+                        .httpStatus(HttpStatus.NOT_FOUND)
+                        .message(exception.getMessage())
+                        .timestamp(LocalDateTime.now())
+                        .path(request.getRequestURI())
+                        .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<HttpErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception,
+                                                                                   HttpServletRequest request) {
+        Map<String, List<String>> errors = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            String field = fieldError.getField();
+            String defaultMessage = fieldError.getDefaultMessage();
+            errors.computeIfAbsent(field, k -> new ArrayList<>()).add(defaultMessage);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(HttpErrorResponse.builder()
+                        .httpStatus(HttpStatus.BAD_REQUEST)
+                        .message("Falló la validación de los campos")
+                        .timestamp(LocalDateTime.now())
+                        .path(request.getRequestURI())
+                        .errors(errors)
+                        .build());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<HttpErrorResponse> handleGenericException(Exception exception, HttpServletRequest request) {
+        log.error("Ocurrió un error inesperado", exception);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(HttpErrorResponse.builder()
+                        .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .message(exception.getMessage())
+                        .timestamp(LocalDateTime.now())
+                        .path(request.getRequestURI())
+                        .build());
+    }
+
+}
---
+++ projects/course-service/src/main/java/dev/magadiflo/course/app/util/HttpErrorResponse.java
@@ -0,0 +1,28 @@
+package dev.magadiflo.course.app.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class HttpErrorResponse {
+    private HttpStatus httpStatus;
+
+    private String message;
+
+    private LocalDateTime timestamp;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String path;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Map<String, List<String>> errors;
+}
