c4de5eef76225072c097763ddabcab8ad176c192
Martín <magadiflo@gmail.com>
2024-09-28 21:06:48 -0500

    Crea dto UserResponse

--- projects/course-service/src/main/java/dev/magadiflo/course/app/model/dto/UserResponse.java
+++ projects/course-service/src/main/java/dev/magadiflo/course/app/model/dto/UserResponse.java
@@ -2,18 +2,15 @@ package dev.magadiflo.course.app.model.dto;
 
 import lombok.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Setter
 @Getter
-public class CourseResponse {
+public class UserResponse {
     private Long id;
     private String name;
-    @Builder.Default
-    private List<UserResponse> users = new ArrayList<>();
+    private String email;
+    private String password;
 }
