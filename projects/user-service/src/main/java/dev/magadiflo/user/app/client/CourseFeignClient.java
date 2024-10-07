34056944ab887375372cc16d245021dae4ef8e93
Martín <magadiflo@gmail.com>
2024-10-07 18:06:02 -0500

    Dockerizando course-service

--- projects/user-service/src/main/java/dev/magadiflo/user/app/client/CourseFeignClient.java
+++ projects/user-service/src/main/java/dev/magadiflo/user/app/client/CourseFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "course-service", url = "http://host.docker.internal:8002", path = "/api/v1/courses")
+@FeignClient(name = "course-service", url = "http://course-service:8002", path = "/api/v1/courses")
 public interface CourseFeignClient {
     @DeleteMapping(path = "/users/{userId}")
     void unassignUserFromAssociatedCourse(@PathVariable Long userId);
