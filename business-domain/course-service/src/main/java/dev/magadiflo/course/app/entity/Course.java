75f669a71f6d2bcdff1322b4048b75dcbe4af13b
Martín Díaz <magadiflo@gmail.com>
2025-05-11 14:14:37 -0500

    Crea relación unidireccional entre Course y CourseUser

--- business-domain/course-service/src/main/java/dev/magadiflo/course/app/entity/Course.java
+++ business-domain/course-service/src/main/java/dev/magadiflo/course/app/entity/Course.java
@@ -1,15 +1,21 @@
 package dev.magadiflo.course.app.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -24,4 +30,8 @@ public class Course {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+    @JoinColumn(name = "course_id")
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CourseUser> courseUsers = new ArrayList<>();
 }
