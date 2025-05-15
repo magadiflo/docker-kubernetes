c804beb79c19aa3af0c01cf26763fd766e34c68f
Martín Díaz <magadiflo@gmail.com>
2025-05-14 23:39:26 -0500

    Agrega nuevo método al UserServiceClient del course-service

--- business-domain/course-service/src/main/java/dev/magadiflo/course/app/client/UserServiceClient.java
+++ business-domain/course-service/src/main/java/dev/magadiflo/course/app/client/UserServiceClient.java
@@ -7,11 +7,13 @@ import dev.magadiflo.course.app.exception.ErrorResponse;
 import dev.magadiflo.course.app.exception.RemoteUserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -57,5 +59,20 @@ public class UserServiceClient {
                 .body(UserResponse.class);
         log.info("Usuario registrado con éxito en el user-service: {}", userResponse);
         return userResponse;
+    }
+
+    public List<UserResponse> getUsersByIdsFromUserService(List<Long> userIds) {
+        log.info("Consultando al servicio user-service por los usuarios con id: {}", userIds);
+        List<UserResponse> userResponseList = this.restClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/by-ids")
+                        .queryParam("userIds", userIds)
+                        .build())
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+        log.info("El servicio user-service encontró a los usuarios: {}", userResponseList);
+        return userResponseList;
     }
 }
